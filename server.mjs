ca7f4c33765b34995875047d9ececb13f07bd94c
AtiDukati <82803562+AtiDukati@users.noreply.github.com>
2023-02-09 19:52:57 +0100

    Stage_3

--- server.mjs
+++ server.mjs
@@ -1,5 +1,7 @@
 import express from "express"; //import express from node_modules
 import Joke from "./node_modules/jokemaster/joke.mjs"; //importere jokes
+//import Dictionary from "./node_modules/languageModul/Dictionary.mjs";
+import TLanguage from "./node_modules/languageModul/module1.mjs";
 
 const server = express();
 
@@ -9,21 +11,28 @@ server.set("port", port);
 
 server.use(express.static("public"));
 
-const jokes = [
-  "How many programmers does it take to change a light bulb? None – It’s a hardware problem",
-  "Why is gravity everywhere? Because it is mass-produced",
-];
+//const jokeTeller = new Joke(new TLanguage("No"));
 
-const jokeTeller = new Joke(jokes);
+server.get("/getJokeNo", (req, res, next) => {
+  //console.log("getting joke");
+  const joke = new Joke(new TLanguage("No")).tellAJoke();
+    //joke = joke.tellAJoke();
 
-server.get("/getJoke", (req, res, next)=>{
-    console.log("getting joke");
-    const joke = jokeTeller.tellAJoke();
-    res.json({joke: joke}).end();
+  res.json({ joke: joke }).end();
 });
+
+server.get("/getJokeEn", (req, res, next) => {
+    //console.log("getting joke");
+    const joke = new Joke(new TLanguage("En")).tellAJoke();
+      //joke = joke.tellAJoke();
+  
+    res.json({ joke: joke }).end();
+  });
+
 
 
 server.listen(server.get("port"), () => {
-  console.log(`server running ${server.get("port")}`);
-  console.log(jokeTeller);
+  //   console.log(`server running ${server.get("port")}`);
+  //   console.log(Dictionary);
+  //console.log(jokeTeller.getJokeIndex());
 });
