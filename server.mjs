f6c2d91ee51268eeabacb4bb20f472fdc48876c0
AtiDukati <82803562+AtiDukati@users.noreply.github.com>
2023-02-14 13:57:44 +0100

    Stage_5

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
-import express from "express"; //import express from node_modules
-import Joke from "./node_modules/jokemaster/joke.mjs"; //importere jokes
+import express from "express";
+import Joke from "./node_modules/jokemaster/joke.mjs";
 import TLanguage from "./node_modules/languageModul/module1.mjs";
 
 const server = express();
@@ -10,27 +10,23 @@ server.set("port", port);
 
 server.use(express.static("public"));
 
-//const jokeTeller = new Joke(new TLanguage("No"));
 
 server.get("/getJokeNo/:index", (req, res, next) => {
-  //console.log("getting joke");
   const lang = TLanguage("No")
   const joke = new Joke(lang[0]).tellAJoke(req.params.index);
-  //joke = joke.tellAJoke();
+  
 
   res.json({ joke: joke, elements: lang[1] }).end();
+  
 });
 
 server.get("/getJokeEn/:index", (req, res, next) => {
   const lang = TLanguage("En")
   const joke = new Joke(lang[0]).tellAJoke(req.params.index);
-  //joke = joke.tellAJoke();
 
   res.json({ joke: joke, elements: lang[1] }).end();
 });
 
 server.listen(server.get("port"), () => {
-  //   console.log(`server running ${server.get("port")}`);
-  //   console.log(Dictionary);
-  //console.log(jokeTeller.getJokeIndex());
+
 });
